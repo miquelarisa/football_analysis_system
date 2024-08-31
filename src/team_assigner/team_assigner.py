d4f0ba09cea0a29390c88792ab68f63579e90ec2
Miquel Arisa <miquelarisafuente7@gmail.com>
2024-08-31 09:04:53 +0200

    team assigner hardcoded id

--- src/team_assigner/team_assigner.py
+++ src/team_assigner/team_assigner.py
@@ -67,7 +67,7 @@ class TeamAssigner:
         team_id = self.kmeans.predict(player_color.reshape(1, -1))[0]
         team_id += 1
 
-        if player_id == 91:
+        if player_id == 91 or player_id == 117:
             team_id = 2
 
         self.player_team_dict[player_id] = team_id
